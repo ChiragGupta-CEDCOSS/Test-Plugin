4160f97ad543a5d9521c16923522b69040409107
ChiragGupta-CEDCOSS <chiraggupta@makewebbetter.com>
2021-05-24 20:04:59 +0530

    remaining files

---
+++ src/subscriptionsForWooCommerce/Test01_ActivateDeactivate.java
@@ -0,0 +1,97 @@
+package subscriptionsForWooCommerce;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Test01_ActivateDeactivate {
+
+	public static void main(String[] args) {
+		
+		// To test the Activation and Deactivation of Subscriptions For WooCommerce Plugin with onboarding and deactivation form working
+		System.setProperty("webdriver.chrome.driver", "/home/cedcoss/MWB Testing Chirag Important/Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		// Maximize Windows
+		driver.manage().window().maximize();
+		
+		// Login admin
+		driver.get("http://localhost:10008/wp-admin/");
+		driver.findElement(By.id("user_login")).sendKeys("root");
+		driver.findElement(By.id("user_pass")).sendKeys("root");
+		driver.findElement(By.id("wp-submit")).click();
+		
+		// Code to get date and time
+		DateTimeFormatter dt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		
+		// Activate plugin
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		driver.findElement(By.cssSelector("a[class='wp-has-submenu wp-not-current-submenu menu-top menu-icon-plugins'] div[class='wp-menu-name']")).click();
+		driver.findElement(By.id("activate-subscriptions-for-woocommerce")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[normalize-space()='Plugin activated.']")));
+		driver.findElement(By.xpath("//p[normalize-space()='Plugin activated.']"));
+		System.out.println("Activation Success message printed");
+		
+		// Check MakeWebBetter Sub-menu
+		Actions actions = new Actions(driver);
+		WebElement MakeWebBetter = driver.findElement(By.xpath("//div[normalize-space()='MakeWebBetter']"));
+		actions.moveToElement(MakeWebBetter).build().perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Subscriptions For WooCommerce']")));
+		
+		//Verifying Subscriptions For WooCommerce Plugin under MakeWebBetter
+		driver.findElement(By.xpath("//a[normalize-space()='Subscriptions For WooCommerce']")).click();
+		System.out.println("Subscriptions For WooCommerce Plugin is present");
+		
+		// Checking On-boarding form
+		try {
+
+			WebElement onboarding = driver.findElement(By.cssSelector(".mwb-sfw-on-boarding-wrapper.mdc-dialog__surface"));
+			if(onboarding.isDisplayed() == true) {
+				driver.findElement(By.xpath("//input[@value='0-500']")).click();
+				driver.findElement(By.id("mwb-sfw-onboard-number")).sendKeys("9058061029");
+				driver.findElement(By.cssSelector("input[value='Send Us']")).click();
+				System.out.println("Onboarding form present and submitted at: " + dt.format(now));
+			}
+		}
+		catch (Exception e){
+			System.out.println("Onboarding form not present");
+			//Deactivate plugin
+			driver.findElement(By.cssSelector("a[class='wp-has-submenu wp-not-current-submenu menu-top menu-icon-plugins'] div[class='wp-menu-name']")).click();
+			driver.findElement(By.id("deactivate-subscriptions-for-woocommerce")).click();
+		}
+		
+		// Check Deactivation form
+		try {
+
+			WebElement deactivation = driver.findElement(By.cssSelector(".mwb-sfw-on-boarding-wrapper.mdc-dialog__surface"));
+			if(deactivation.isDisplayed() == true) {
+				driver.findElement(By.xpath("//input[@value='temporary-deactivation-for-debug']")).click();
+				driver.findElement(By.xpath("//textarea[@id='mwb-sfw-deactivation-reason-text']")).sendKeys("Chirag Gupta QA");
+				driver.findElement(By.cssSelector("input[value='Send Us']")).click();
+				System.out.println("Deactivation form present and submitted at: " + dt.format(now));
+				
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[normalize-space()='Plugin deactivated.']")));
+				driver.findElement(By.xpath("//p[normalize-space()='Plugin deactivated.']"));
+				System.out.println("Deactivation Success message printed");
+				
+			}
+
+		}
+		catch (Exception e){
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[normalize-space()='Plugin deactivated.']")));
+			driver.findElement(By.xpath("//p[normalize-space()='Plugin deactivated.']"));
+			System.out.println("Deactivation Success message printed");
+			System.out.println("Deactivation form not present");
+		}
+
+	}
+
+}
---
+++ src/subscriptionsForWooCommerce/Test02_AcceptanceTesting.java
@@ -0,0 +1,137 @@
+package subscriptionsForWooCommerce;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Test02_AcceptanceTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// To test the Activation and Deactivation of Subscriptions For WooCommerce Plugin with onboarding and deactivation form working
+		System.setProperty("webdriver.chrome.driver", "/home/cedcoss/MWB Testing Chirag Important/Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		// Maximize Windows
+		driver.manage().window().maximize();
+		
+		// Login admin
+		driver.get("http://localhost:10008/wp-admin/");
+		driver.findElement(By.id("user_login")).sendKeys("root");
+		driver.findElement(By.id("user_pass")).sendKeys("root");
+		driver.findElement(By.id("wp-submit")).click();
+		
+		// Activate plugin
+		WebDriverWait wait = new WebDriverWait(driver,30);
+		driver.findElement(By.cssSelector("a[class='wp-has-submenu wp-not-current-submenu menu-top menu-icon-plugins'] div[class='wp-menu-name']")).click();
+		driver.findElement(By.id("activate-subscriptions-for-woocommerce")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[normalize-space()='Plugin activated.']")));
+		driver.findElement(By.xpath("//p[normalize-space()='Plugin activated.']"));
+		System.out.println("Activation Success message printed");
+		
+		Actions actions = new Actions(driver);
+		WebElement MakeWebBetter = driver.findElement(By.xpath("//div[normalize-space()='MakeWebBetter']"));
+		actions.moveToElement(MakeWebBetter).build().perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Subscriptions For WooCommerce']")));
+		
+		//Verifying Subscriptions For WooCommerce Plugin under MakeWebBetter
+		driver.findElement(By.xpath("//a[normalize-space()='Subscriptions For WooCommerce']")).click();
+		System.out.println("Subscriptions For WooCommerce Plugin is present under MakeWebBetter");
+		
+		driver.findElement(By.xpath("//h1[normalize-space()='SUBSCRIPTIONS FOR WOOCOMMERCE']")).isDisplayed();
+		System.out.println("SUBSCRIPTIONS FOR WOOCOMMERCE text is present of plugin");
+		
+		driver.findElement(By.xpath("//a[@class='mwb-link']")).isDisplayed();
+		System.out.println("Support link is present");
+		
+		driver.findElement(By.id("subscriptions-for-woocommerce-overview")).isDisplayed();
+		System.out.println("Overview Tab is present");
+		
+		driver.findElement(By.id("subscriptions-for-woocommerce-general")).isDisplayed();
+		System.out.println("General Setting Tab is present");
+		
+		driver.findElement(By.id("subscriptions-for-woocommerce-subscriptions-table")).isDisplayed();
+		System.out.println("Subscriptions Table Tab is present");
+		
+		driver.findElement(By.id("subscriptions-for-woocommerce-system-status")).isDisplayed();
+		System.out.println("System status Tab is present");
+
+		// Verifying General settings availability
+		driver.findElement(By.id("subscriptions-for-woocommerce-general")).click();
+
+		driver.findElement(By.xpath("//label[normalize-space()='Enable/Disable Subscription']")).isDisplayed();
+		driver.findElement(By.xpath("//label[normalize-space()='Add to cart text']")).isDisplayed();
+		driver.findElement(By.xpath("//label[normalize-space()='Place order text']")).isDisplayed();
+		driver.findElement(By.xpath("//label[normalize-space()='Allow Customer to cancel Subscription']")).isDisplayed();
+		driver.findElement(By.xpath("//label[normalize-space()='Check this box to enable the subscription.']")).isDisplayed();
+		driver.findElement(By.xpath("//label[contains(text(),'Enable this option to allow the customer to cancel')]")).isDisplayed();
+		driver.findElement(By.xpath("//input[@placeholder='Add to cart button text']")).isDisplayed();
+		driver.findElement(By.xpath("//input[@placeholder='Place order button text']")).isDisplayed();
+		driver.findElement(By.xpath("//div[contains(text(),'Use this option to change add to cart button text.')]")).isDisplayed();
+		driver.findElement(By.xpath("//div[contains(text(),'Use this option to change place order button text.')]")).isDisplayed();
+		boolean addtocart = driver.findElement(By.id("mwb_sfw_enable_plugin")).isSelected();
+		if(addtocart = false) {
+			driver.findElement(By.id("mwb_sfw_enable_plugin")).click();
+			driver.findElement(By.id("//span[@class='mdc-button__ripple']")).click();
+			System.out.println("Add To Cart was unchecked and now checked");
+		} else {
+			driver.findElement(By.id("mwb_sfw_enable_plugin")).click();
+			System.out.println("Add To Cart was checked and now unchecked");
+		}
+		
+		driver.findElement(By.id("mwb_sfw_cancel_subscription_for_customer")).isDisplayed();
+		if(addtocart = false) {
+			driver.findElement(By.id("mwb_sfw_cancel_subscription_for_customer")).click();
+			driver.findElement(By.id("//span[@class='mdc-button__ripple']")).click();
+			System.out.println("Allow cancel subscription was unchecked and now checked");
+		} else {
+			driver.findElement(By.id("mwb_sfw_cancel_subscription_for_customer")).click();
+			System.out.println("Allow cancel subscription checked and now unchecked");
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//Deactivate plugin code
+		driver.findElement(By.cssSelector("a[class='wp-has-submenu wp-not-current-submenu menu-top menu-icon-plugins'] div[class='wp-menu-name']")).click();
+		driver.findElement(By.id("deactivate-subscriptions-for-woocommerce")).click();
+		try {
+
+			WebElement deactivation = driver.findElement(By.cssSelector(".mwb-sfw-on-boarding-wrapper.mdc-dialog__surface"));
+			if(deactivation.isDisplayed() == true) {
+				driver.findElement(By.xpath("//a[normalize-space()='Skip and Deactivate Now']")).click();
+				System.out.println("Deactivation form present");
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[normalize-space()='Plugin deactivated.']")));
+				driver.findElement(By.xpath("//p[normalize-space()='Plugin deactivated.']"));
+				System.out.println("Deactivation Success message printed");
+				
+			}
+
+		}
+		catch (Exception e){
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[normalize-space()='Plugin deactivated.']")));
+			driver.findElement(By.xpath("//p[normalize-space()='Plugin deactivated.']"));
+			System.out.println("Deactivation Success message printed");
+			System.out.println("Deactivation form not present");
+		}
+	}
+
+}
